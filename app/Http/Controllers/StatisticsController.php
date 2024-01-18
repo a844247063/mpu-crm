986fc132574de1980d27482c753476ff7b4046bb
Wade Pak <P2001151@mpu.edu.mo>
2024-01-18 16:57:54 +0800

    see my work

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -14,59 +12,22 @@ use Carbon\Carbon;
 
 class StatisticsController extends Controller
 {
-    public function __construct()
-    {
-        //$this->authorizeResource(Department::class);
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function Statistics()
     {
-        $enquiryAll = Enquiry::all();         
-  
-        $enquiriesWithHour = $enquiryAll->map(function ($enquiry) {      
+        $enquiryAll = Enquiry::all();
+
+        $enquiriesWithHour = $enquiryAll->map(function ($enquiry) {
             $hour = Carbon::parse($enquiry->created_at)->format('H');
             $enquiry->hour = $hour;
-    
+
             return $enquiry;
         });
-        
-    
+
         return Inertia::render('Statistics', [
             'enquiriesWithHour' => $enquiriesWithHour,
-            'enquiryAll' => $enquiryAll, 
-            'fields' => Config::enquiryFormFields(), 
+            'enquiryAll' => $enquiryAll,
+            'fields' => Config::enquiryFormFields(),
         ]);
     }
 
-
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-   
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
- 
-
-
-   
-
-    
 }
--- resources/js/Pages/Statistics.vue
+++ resources/js/Pages/Statistics.vue
@@ -1,59 +1,83 @@
 <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
-</template>
+    <div class="chart-container">
+      <Bar
+        id="chart1"
+        :options="chartOptions"
+        :data="chartData"
+      />
+      <Bar
+        id="chart2"
+        :options="chartOptions"
+        :data="chartData"
+      />
+    </div>
+  </template>
 
-<script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-import { ref } from 'vue'
+  <style>
+  .chart-container {
+    display: flex;
+    flex-wrap: wrap; /* 允许容器内的项目在必要时换行 */
+    justify-content: space-around;
+  }
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  .chart-container > div {
+    flex: 1; /* 使得每个图表占据可用空间 */
+    min-width: 300px; /* 设定图表的最小宽度，当容器太小无法容纳两个图表时会换行 */
+  }
 
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  props: ['enquiriesWithHour','fields'],
-  setup(props) {
-    const chartData = ref({
-      labels: [],
-      datasets: [
-        {
-          label: 'Enquiries per Hour',
-          backgroundColor: '#f87979',
-          data: []
-        }
-      ]
-    })
+  @media (max-width: 600px) {
+    .chart-container {
+      flex-direction: column; /* 当屏幕宽度小于600px时，改变排列方向为垂直 */
+    }
+  }
+  </style>
 
-    const chartOptions = {
-      responsive: true,
-      plugins: {
-        legend: {
-          display: false
+
+  <script>
+  import { Bar } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  import { ref } from 'vue'
+
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+  export default {
+    name: 'BarCharts',
+    components: { Bar },
+    props: ['enquiriesWithHour', 'fields'],
+    setup(props) {
+      const chartData = ref({
+        labels: [],
+        datasets: [
+          {
+            label: 'Enquiries per Hour',
+            backgroundColor: '#f87979',
+            data: []
+          }
+        ]
+      })
+
+      const chartOptions = {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          }
         }
       }
+
+      const processEnquiries = () => {
+        const counts = new Array(24).fill(0);
+        props.enquiriesWithHour.forEach(enquiry => {
+          const hour = parseInt(enquiry.hour);
+          counts[hour]++;
+        });
+        chartData.value.labels = [...Array(24).keys()].map(hour => `${hour}:00`);
+        chartData.value.datasets[0].data = counts;
+      }
+
+      processEnquiries();
+
+      return { chartData, chartOptions }
     }
-
-    const processEnquiries = () => {
-      // 创建一个数组来存储每小时的计数
-      const counts = new Array(24).fill(0);
-      props.enquiriesWithHour.forEach(enquiry => {
-        // 使用服务器发送的小时数
-        const hour = parseInt(enquiry.hour);
-        counts[hour]++; // 增加对应小时的计数
-      });
-      // 更新chartData
-      chartData.value.labels = [...Array(24).keys()].map(hour => `${hour}:00`);
-      chartData.value.datasets[0].data = counts;
-    }
-
-    processEnquiries();
-
-    return { chartData, chartOptions }
   }
-}
-</script>
+  </script>
